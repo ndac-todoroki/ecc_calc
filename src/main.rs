5c15e127cec8fedbac52fec86e89400ba0dc37b7
Todoroki <todoroki.auone@gmail.com>
2018-03-06 15:49:52 +0900

    edit test

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,16 @@ extern crate simple_logger;
 
 // test data http://point-at-infinity.org/ecc/nisttv
 
-fn main() {
-   use module::ecc::prime::points::affine::{AffinePoint, NewPoint as NewAffine};
-   use module::ecc::prime::points::jacobian::{JacobianPoint, NewPoint as NewJacobian};
-   use module::ecc::prime::ECCurvePoint;
-   use module::ecc::prime::curves::{ECCurve, ECCurveCalculation, Secp256r1};
-   use module::ecc::ECCValue;
-   use std::error::Error;
+use module::ecc::prime::points;
+use module::ecc::prime::points::affine::{AffinePoint, NewPoint as NewAffine};
+use module::ecc::prime::points::jacobian::{JacobianPoint, NewPoint as NewJacobian};
+use module::ecc::prime::ECCurvePoint;
+use module::ecc::prime::curves::{ECCurve, ECCurveCalculation, Secp256k1, Secp256r1};
+use module::ecc::ECCValue;
+use std::error::Error;
+use std::marker::Sized;
 
+fn main() {
    simple_logger::init().unwrap();
 
    let point = AffinePoint::try_new(
@@ -37,7 +39,7 @@ fn main() {
          println!("{:X}", point);
       },
       false => println!("Could not parse {:?}", point),
-   }
+   };
 
    println!("\n{}", "Affine -> Jacobian");
    let jacob = curve.convert_point_to::<JacobianPoint>(&point);
@@ -57,91 +59,99 @@ fn main() {
       Err(err) => println!("Could not parse {:?}", err.cause()),
    }
 
-   // Jacobian addition test
-   /*
-   # G + G == 2*G
-   a = 18905f76a53755c679fb732b7762251075ba95fc5fedb60179e730d418a9143c, 8571ff1825885d85d2e88688dd21f3258b4ab8e4ba19e45cddf25357ce95560a, 00000000fffffffeffffffffffffffffffffffff000000000000000000000001
-   b = 18905f76a53755c679fb732b7762251075ba95fc5fedb60179e730d418a9143c, 8571ff1825885d85d2e88688dd21f3258b4ab8e4ba19e45cddf25357ce95560a, 00000000fffffffeffffffffffffffffffffffff000000000000000000000001
-   r = f6bb32e43dcf3a3b732205038d1490d9aa6ae3c1a433827d850046d410ddd64d, 78c577510a5b8a3b19a8fb0e92042dbe152cd7cbeb236ff82f3648d361bee1a5
-   */
+   let secp256k1 = Curves::Secp256k1;
+   let secp256r1 = Curves::Secp256r1;
 
-   println!("\n{}", "G + G = 2G test");
-   let point2 = JacobianPoint::try_new(
-      "18905f76a53755c679fb732b7762251075ba95fc5fedb60179e730d418a9143c",
-      "8571ff1825885d85d2e88688dd21f3258b4ab8e4ba19e45cddf25357ce95560a",
-      "00000000fffffffeffffffffffffffffffffffff000000000000000000000001",
-      16,
-   ).unwrap();
+   baseG_multipy_2_test(&secp256r1);
+   baseG_multipy_n_test(&secp256r1);
 
-   /* println!("{}", "Check if point G is on curve");
-   match curve.point_is_on_curve(&point2) {
-      true => {
-         println!(
-            "{:X}",
-            curve.convert_point_to::<AffinePoint>(&point2).unwrap()
-         );
+   baseG_multipy_2_test(&secp256k1);
+   baseG_multipy_n_test(&secp256k1);
+}
 
-         let point2p2 = curve.add_points(&point2, &point2);
-         println!("{:x}", point2p2);
-         println!(
-            "2G= {:x}",
-            curve.convert_point_to::<AffinePoint>(&point2p2).unwrap()
-         );
+enum Curves {
+   Secp256k1,
+   Secp256r1,
+}
 
-         let point_R = AffinePoint::try_new(
-            "f6bb32e43dcf3a3b732205038d1490d9aa6ae3c1a433827d850046d410ddd64d",
-            "78c577510a5b8a3b19a8fb0e92042dbe152cd7cbeb236ff82f3648d361bee1a5",
-            16,
-         ).unwrap();
-         println!("\nPoint r = {:x}", point_R);
+fn baseG_multipy_2_test(curve_enum: &Curves) {
+   let curve = match curve_enum {
+      &Curves::Secp256k1 => {
+         let curve = Secp256k1::new();
+         println!("\nG, 2G test 2 on {}", curve.name());
+         let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
+         println!("G={:x}", &curve.base_point());
+         match point_G {
+            Ok(point) => {
+               println!("G= {:x}", point);
+               let point_2G = curve.multipy_point(&point, BigInt::from(2));
+               println!("2G= {:x}", point_2G);
+               println!(
+                  "2G= {:x}",
+                  curve.convert_point_to::<AffinePoint>(&point_2G).unwrap()
+               )
+            },
+            _ => (),
+         }
       },
-      false => println!("Could not parse {:?}", point2),
-   } */
-
-   println!("\n{}", "G, 2G test");
-   let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
-   println!("G= {:x}", &curve.base_point());
-   match point_G {
-      Ok(point) => {
-         println!("G= {:x}", point);
-         let point_2G = curve.double_point(&point);
-         println!(
-            "2G= {:x}",
-            curve.convert_point_to::<AffinePoint>(&point_2G).unwrap()
-         )
+      &Curves::Secp256r1 => {
+         let curve = Secp256r1::new();
+         println!("\nG, 2G test 2 on {}", curve.name());
+         let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
+         println!("G={:x}", &curve.base_point());
+         match point_G {
+            Ok(point) => {
+               println!("G= {:x}", point);
+               let point_2G = curve.multipy_point(&point, BigInt::from(2));
+               println!("2G= {:x}", point_2G);
+               println!(
+                  "2G= {:x}",
+                  curve.convert_point_to::<AffinePoint>(&point_2G).unwrap()
+               )
+            },
+            _ => (),
+         }
       },
-      _ => (),
-   }
+   };
+}
 
-   println!("\n{}", "G, 2G test 2");
-   let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
-   println!("G= {:x}", &curve.base_point());
-   match point_G {
-      Ok(point) => {
-         println!("G= {:x}", point);
-         let point_2G = curve.multipy_point(&point, BigInt::from(2));
-         println!("2G= {:x}", point_2G);
-         println!(
-            "2G= {:x}",
-            curve.convert_point_to::<AffinePoint>(&point_2G).unwrap()
-         )
+fn baseG_multipy_n_test(curve_enum: &Curves) {
+   match curve_enum {
+      &Curves::Secp256k1 => {
+         let curve = Secp256k1::new();
+         println!("\nn * G should be inf test on {}", curve.name());
+         let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
+         let n = curve.n();
+         match point_G {
+            Ok(point) => {
+               let point_R = curve.multipy_point(&point, n);
+
+               println!("R= {:x}", point_R);
+               println!(
+                  "R= {:x}",
+                  curve.convert_point_to::<AffinePoint>(&point_R).unwrap()
+               );
+            },
+            _ => (),
+         }
       },
-      _ => (),
-   }
+      &Curves::Secp256r1 => {
+         let curve = Secp256r1::new();
+         println!("\nn * G should be inf test on {}", curve.name());
+         let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
+         let n = curve.n();
+         match point_G {
+            Ok(point) => {
+               let point_R = curve.multipy_point(&point, n);
 
-   println!("\n{}", "n * G should be inf test");
-   let point_G = curve.convert_point_to::<JacobianPoint>(&curve.base_point());
-   let n = curve.n();
-   match point_G {
-      Ok(point) => {
-         let point_R = curve.multipy_point(&point, n);
-
-         println!("R= {:x}", point_R);
-         println!(
-            "R= {:x}",
-            curve.convert_point_to::<AffinePoint>(&point_R).unwrap()
-         );
+               println!("R= {:x}", point_R);
+               println!(
+                  "R= {:x}",
+                  curve.convert_point_to::<AffinePoint>(&point_R).unwrap()
+               );
+            },
+            _ => (),
+         }
       },
-      _ => (),
-   }
+   };
 }
